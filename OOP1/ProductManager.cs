03fcae2be40785838934ca0765d85ae2decd46de
uczburak <uczbrk@gmail.com>
2022-03-06 22:39:59 +0300

    Class 4(OOP1), until to the 2. break.

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        public void Add(Product product)
+        {
+            Console.WriteLine("Product Added : " + product.ProductName);
+        }
+        
+        public void Update(Product product)
+        {
+            Console.WriteLine("Product Updated : " + product.ProductName);
+        }
+
+        public void Delete(Product product)
+        {
+            Console.WriteLine("Product Deleted : " + product.ProductName);
+        }
+    }
+}
