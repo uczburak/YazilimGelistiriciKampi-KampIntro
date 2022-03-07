66bfd84eb02705da94a6b7b460ea5da4b0d98e49
uczburak <uczbrk@gmail.com>
2022-03-07 12:22:27 +0300

    Class 4(4th day) - End of the chapter.

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsIntro
+{
+    class MyList<T>
+    {
+        T[] items;
+        public MyList()
+        {
+            items = new T[0];
+        }
+
+        public void Add(T item)
+        {
+            T[] tempArr = items;
+            items = new T[items.Length + 1];
+            for (int i = 0; i < tempArr.Length; i++)
+            {
+                items[i] = tempArr[i];
+            }
+
+            items[items.Length - 1] = item;
+        }
+
+        public int Count
+        {
+            get { return items.Length; }
+        }
+
+        public T[] Items
+        {
+            get { return items; }
+        }
+    }
+}
---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> names = new();
+
+            names.Add("Burak");
+
+            Console.WriteLine(names.Count);
+
+            names.Add("Osman");
+
+            names.Add("Pidor");
+
+            names.Add("Hakan");
+
+            Console.WriteLine(names);
+
+
+            foreach (string name in names.Items)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
