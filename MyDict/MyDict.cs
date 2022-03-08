93be175c95ef40702c82603a7053ec991eb93a39
uczburak <uczbrk@gmail.com>
2022-03-08 23:07:26 +0300

    Added MyDict Project which is adding, deleting by key parameter.

---
+++ MyDict/MyDict.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Linq;
+
+namespace MyDict
+{
+    
+
+    class MyDict<TKey, TValue>
+    {
+        TKey[] keys;
+        TValue[] values;
+
+        public MyDict()
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+        }
+
+
+        public void Add(TKey key, TValue value)
+        {
+            if (!keys.Contains(key))
+            {
+                TKey[] tempKey = keys;
+                TValue[] tempValues = values;
+
+                keys = new TKey[keys.Length + 1];
+                values = new TValue[values.Length + 1];
+
+                for (int i = 0; i < tempKey.Length; i++)
+                {
+                    keys[i] = tempKey[i];
+                    values[i] = tempValues[i];
+                }
+
+                keys[keys.Length - 1] = key;
+                values[values.Length - 1] = value;
+            }
+            else
+                Console.WriteLine(key + " Anahtar Kelimesine ait zaten bir eleman var.");
+        }
+
+        public int Count
+        {
+            get { return keys.Length; }
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Console.WriteLine(keys[i] + ": " + values[i]);
+            }
+        }
+
+        public void Delete(TKey key)
+        {
+            if (keys.Length != 0)
+            {
+                TKey[] tempKey = keys;
+                TValue[] tempValues = values;
+
+                keys = new TKey[keys.Length - 1];
+                values = new TValue[values.Length - 1];
+                int i = 0;
+                int index = 0;
+
+                for (i = 0; i < tempKey.Length; i++)
+                {
+                    if (tempKey[i].ToString() == key.ToString())
+                    {
+                        index = i;
+                        break;
+                    }
+
+                }
+                for ( i = 0; i < tempKey.Length; i++)
+                {
+                    if(i < index)
+                    {
+                        keys[i] = tempKey[i];
+                        values[i] = tempValues[i];
+                    }
+                    if(i > index)
+                    {
+                        keys[i-1] = tempKey[i];
+                        values[i-1] = tempValues[i];
+                    }
+                }
+                //i = 0;
+                //int j = 0;
+                //while(i<keys.Length)
+                //{
+                //    if (j != index)
+                //    {
+                //        keys[i] = tempKey[j];
+                //        values[i] = tempValues[j];
+                //        i++;
+                //    }
+                //    j++;
+                //}
+
+            }
+        }
+
+        public void Delete()
+        {
+            if (keys.Length != 0)
+            {
+                TKey[] tempKey = keys;
+                TValue[] tempValues = values;
+
+                keys = new TKey[tempKey.Length - 1];
+                values = new TValue[tempValues.Length - 1];
+
+                for (int i = 0; i < keys.Length; i++)
+                {
+                    keys[i] = tempKey[i];
+                    values[i] = tempValues[i];
+                }
+            }
+            else
+                Console.WriteLine("Cannot delete an empty Dictionary.");
+        }
+    }
+}
---
+++ MyDict/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyDict
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            MyDict<int, string> myDict = new MyDict<int, string>();
+
+            myDict.Add(1, "osman");
+            myDict.Add(2, "burak");
+            myDict.Add(1, "Burak");
+            myDict.Add(4, "berkan");
+            myDict.Add(5, "emir");
+            myDict.Print();
+            myDict.Delete(4);
+            Console.WriteLine(myDict.Count);
+            myDict.Print();
+            
+        }
+
+    }
+}
